b8edbcc116bcd30ca65d63232dcc0bb40ba4eb52
Adrian Sztandera <sztandera.adrian@gmail.com>
2022-07-04 18:39:01 +0200

    -add new elements

--- index.js
+++ index.js
@@ -1,25 +1,83 @@
 var clickBtnToDo = document.getElementById("clickBtnToDo");
-var displayJobs = document.querySelector(".displayJobs");
+var displayJobs = document.getElementById(".displayJobs");
 var displayType = document.getElementById('displayType')
 var displayCost = document.getElementById('displayCost')
 var country = document.getElementById('country')
+var manyElems = Array.from(document.querySelectorAll(".element"))
+let currentPosition = 1
+let click =0;
+let clicked = document.getElementById('clicked')
+
 
 var whatCanI = async() =>{
     let response = await fetch("https://apis.scrimba.com/bored/api/activity");
     let responseToDo = await response.json();
     return responseToDo;
 }
+var showElements = (currentPosition,previousPosition)=>{
+    let testValue = document.getElementById("displayCost");
+    if(currentPosition === 0){
+        previousPosition = 2
+    }
+    if(testValue !== null){
+        console.log(displayJobs)
+        manyElems[previousPosition].removeChild(displayJobs)
+        manyElems[previousPosition].removeChild(displayType)
+        manyElems[previousPosition].removeChild(displayCost)
+    }
+
+    displayJobs = document.createElement('div');
+    displayCost = document.createElement('div');
+    displayType = document.createElement('div');
+
+    displayJobs.setAttribute("id", "displayJobs");
+    displayType.setAttribute("id", "displayType");
+    displayCost.setAttribute("id", "displayCost");
+    displayJobs.setAttribute("class", "displayJobs -inElement");
+    displayType.setAttribute("class", "displayType -inElement");
+    displayCost.setAttribute("class", "displayCost -inElement");
+
+    manyElems[previousPosition].classList.remove("current");
+    
+    manyElems[currentPosition].classList.add("current");
+
+    manyElems[currentPosition].appendChild(displayJobs)
+    manyElems[currentPosition].appendChild(displayType)
+    manyElems[currentPosition].appendChild(displayCost)
+    
+}
+
+var clickedBtn = ()=>{
+    if(click===0){
+        clicked.classList.add("current");
+        clicked.classList.add("-clicked");
+    }
+    click++
+    clicked.innerHTML = `Clicked ${click}`
+}
+
 
 var toDo = ()=>{
+    let previousPosition = currentPosition -1
+
+    showElements(currentPosition, previousPosition)
+    clickedBtn()
     whatCanI().then(data => {
-        let displayCostValue = data.price === 0 ? "Nothing" 
-        : data.price < 0.2 ? "Something" 
-        : "Do not think only do"
+        let displayCostValue = data.price === 0 ? "Do..." 
+        : data.price < 0.2 ? "Why not?" 
+        : "Do not think only do!"
         displayJobs.innerHTML = data.activity
         displayType.innerHTML = data.type
         displayCost.innerHTML = displayCostValue
-        console.log(data.price)
     })
+    currentPosition++
+    if(currentPosition > 2){
+        currentPosition = 0
+    }
 }
+
+
+
+
 
 clickBtnToDo.addEventListener("click",toDo)
