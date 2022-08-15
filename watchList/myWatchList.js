9a5d46960e4fa8d749593c31062d19909a3f4d04
Adrian Sztandera <sztandera.adrian@gmail.com>
2022-08-15 13:04:27 +0200

    -add new element

---
+++ watchList/myWatchList.js
@@ -0,0 +1,2 @@
+const myList = JSON.parse(localStorage.getItem('myList'));
+console.log(myList)
--- watchList/watchList.js
+++ watchList/watchList.js
@@ -4,12 +4,84 @@
 // https://www.omdbapi.com/?apikey=1d5608e7&t=batman&y=2000&plot=full  na dany rok
 // https://www.omdbapi.com/?apikey=1d5608e7&t=batman&plot=full all
 
-var shameApiColor = async() =>{
-     let response = await fetch(`https://www.omdbapi.com/?apikey=1d5608e7&t=batman&plot=full`);
-    let responseToDo = await response.json();
-    return responseToDo;
+const searcBtn = document.getElementById('searcBtn')
+const searchList = document.getElementById('searchList')
+var myList = []
+
+
+//take 10 element from api
+const apiVideoSearch = async () => {
+    videoName = document.getElementById('searchVideo').value
+    let response = await fetch(`https://www.omdbapi.com/?apikey=1d5608e7&s=${videoName}`);
+    let apiVideoSearch = await response.json();
+    return apiVideoSearch;
 }
 
-window.onload = function() {
-    shameApiColor().then(data =>console.log(data))
+//take specific option video from api
+const specificVideoApi = async (imdbID) => {
+    let response = await fetch(`http://www.omdbapi.com/?apikey=1d5608e7&i=${imdbID}&plot=Short`);
+    let specificVideoApi = await response.json();
+    return specificVideoApi;
+}
+
+
+
+
+
+
+//search btn to add id video 
+const searchVideoToMyList = () => {
+    const buttons = document.querySelectorAll(".addVideo");
+    for (const button of buttons) {
+        //add id to array myList
+        button.addEventListener('click', function (event) {
+            var newElementValue = event.target.value
+            console.log(myList.includes(newElementValue))
+            if (myList.includes(newElementValue)){
+                myList = myList.filter(item => item !== newElementValue)
+            }else{
+                myList.push(newElementValue)
+            }
+            newMyList = JSON.stringify(myList)
+            localStorage.setItem('myList', newMyList);
+        })
+    }
+}
+
+
+const giveMeVideo = () => {
+    html = ''
+    apiVideoSearch().then(data => {
+        data.Search.map((dateValue, i, arr) => {
+            specificVideoApi(dateValue.imdbID).then(specificVideo => {
+                html += `
+                <div class="serachList__oneVideo">
+                    <img class="fit-picture"
+                    src=${specificVideo.Poster}
+                    alt=${specificVideo.Title}>
+                    <div class="serachList__oneVideo--contenet">
+                        <div class="serachList__oneVideo--header"> 
+                            <h1>${specificVideo.Title}</h1>
+                            <svg class="star"width="13" height="12" viewBox="0 0 13 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M5.78671 1.19529C6.01122 0.504306 6.98878 0.504305 7.21329 1.19529L8.01547 3.66413C8.11588 3.97315 8.40384 4.18237 8.72876 4.18237H11.3247C12.0512 4.18237 12.3533 5.11208 11.7655 5.53913L9.66537 7.06497C9.40251 7.25595 9.29251 7.59447 9.39292 7.90349L10.1951 10.3723C10.4196 11.0633 9.62875 11.6379 9.04097 11.2109L6.94084 9.68503C6.67797 9.49405 6.32203 9.49405 6.05916 9.68503L3.95903 11.2109C3.37125 11.6379 2.58039 11.0633 2.8049 10.3723L3.60708 7.90349C3.70749 7.59448 3.59749 7.25595 3.33463 7.06497L1.2345 5.53914C0.646715 5.11208 0.948796 4.18237 1.67534 4.18237H4.27124C4.59616 4.18237 4.88412 3.97315 4.98453 3.66414L5.78671 1.19529Z" fill="#FEC654"/>
+                            </svg>
+                            <p class="ratio">${specificVideo.imdbRating}</p>
+                        </div>
+                        <div  class="serachList__oneVideo--options">
+                            <p>${specificVideo.Runtime}</p>
+                            <p>${specificVideo.Genre}</p>
+                            <button class="addVideo" type="submit" value=${dateValue.imdbID}>Add watclist</button>
+                        </div>
+                            <p>${specificVideo.Plot}</p>
+                    </div>
+                </div>`
+                searchList.innerHTML = html
+                //last element in array
+                if (arr.length - 1 !== i) return
+                searchVideoToMyList()
+            })
+        })
+    })
 };
+
+searcBtn.addEventListener("click", giveMeVideo);
